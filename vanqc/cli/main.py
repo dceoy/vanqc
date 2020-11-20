c9bcb96beb38f9bc78798d748864524912bf69d9
dceoy <dnarsil+github@gmail.com>
2020-11-21 04:33:14 +0900

    Implement stats and metrics commands

--- vanqc/cli/main.py
+++ vanqc/cli/main.py
@@ -18,6 +18,10 @@ Usage:
     vanqc funcotatesegments [--debug|--info] [--cpus=<int>] [--skip-cleaning]
         [--ref-ver=<str>] [--dest-dir=<path>] <data_dir_path>
         <fa_path> <seg_path>...
+    vanqc stats [--debug|--info] [--cpus=<int>] [--skip-cleaning]
+        [--dest-dir=<path>] <fa_path> <vcf_path>...
+    vanqc metrics [--debug|--info] [--cpus=<int>] [--skip-cleaning]
+        [--dest-dir=<path>] <fa_path> <dbsnp_vcf_path> <vcf_path>...
     vanqc -h|--help
     vanqc --version
 
@@ -27,6 +31,8 @@ Commands:
     snpeff                  Annotate VCF files using SnpEff
     funcotator              Annotate VCF files using GATK Funcotator
     funcotatesegments       Annotate SEG files using GATK FuncotateSegments
+    stats                   Collect VCF stats using Bcftools
+    metrics                 Collect variant calling metrics using GATK (Picard)
 
 Options:
     -h, --help              Print help and exit
@@ -48,6 +54,7 @@ Args:
     <vcf_path>              Path to a VCF file
     <data_dir_path>         Path to a data source directory
     <seg_path>              Path to a segment TSV files
+    <dbsnp_vcf_path>        Path to a reference dbSNP file
 """
 
 import logging
@@ -59,10 +66,11 @@ from docopt import docopt
 from psutil import cpu_count, virtual_memory
 
 from .. import __version__
-from ..task.bcftools import NormalizeVcf
+from ..task.bcftools import CollectVcfStats, NormalizeVcf
 from ..task.gatk import (AnnotateSegWithFuncotateSegments,
                          AnnotateVcfWithFuncotator,
                          DownloadFuncotatorDataSources)
+from ..task.picard import CollectVariantCallingMetrics
 from ..task.snpeff import AnnotateVcfWithSnpeff, DownloadSnpeffDataSources
 from .builder import build_luigi_tasks
 from .util import fetch_executable, print_log
@@ -84,7 +92,6 @@ def main():
     logger.debug(f'args:{os.linesep}{args}')
     assert args['--ref-ver'] not in {'hg38', 'hg19'}, 'invalid ref version'
     print_log(f'Start the workflow of vanqc {__version__}')
-    dest_dir = Path(args['--dest-dir']).resolve()
     n_cpu = int(args['--cpus'] or cpu_count())
     memory_mb = virtual_memory().total / 1024 / 1024 / 2
     if args['download']:
@@ -102,7 +109,7 @@ def main():
                 (
                     [
                         DownloadSnpeffDataSources(
-                            dest_dir_path=str(dest_dir), snpeff=snpeff,
+                            dest_dir_path=args['--dest-dir'], snpeff=snpeff,
                             genome_version={
                                 'hg38': 'GRCh38', 'hg19': 'GRCh37'
                             }[args['--ref-ver']],
@@ -112,7 +119,7 @@ def main():
                 ) + (
                     [
                         DownloadFuncotatorDataSources(
-                            dest_dir_path=str(dest_dir), gatk=gatk,
+                            dest_dir_path=args['--dest-dir'], gatk=gatk,
                             **{
                                 c: fetch_executable(c)
                                 for c in ['pigz', 'pbzip2']
@@ -130,38 +137,51 @@ def main():
         )
         n_worker = min(n_target, n_cpu)
         common_kwargs = {
-            'fa_path': args['<fa_path>'], 'dest_dir_path': str(dest_dir),
-            'n_cpu': (floor(n_cpu / n_target) if n_cpu > n_target else 1),
-            'memory_mb':
-            int(virtual_memory().total / 1024 / 1024 / n_worker),
+            'fa_path': args['<fa_path>'], 'dest_dir_path': args['--dest-dir'],
+            'n_cpu': max(floor(n_cpu / n_worker), 1),
+            'memory_mb': (memory_mb / n_worker),
             'remove_if_failed': (not args['--skip-cleaning'])
         }
-        bcftools_path = fetch_executable('bcftools')
+        bcftools = fetch_executable('bcftools')
         if args['normalize']:
-            kwargs = {'bcftools': bcftools_path, **common_kwargs}
+            kwargs = {'bcftools': bcftools, **common_kwargs}
             build_luigi_tasks(
                 tasks=[
-                    NormalizeVcf(
-                        input_vcf_path=str(Path(p).resolve()), **kwargs
-                    ) for p in args['<vcf_path>']
+                    NormalizeVcf(input_vcf_path=p, **kwargs)
+                    for p in args['<vcf_path>']
                 ],
                 workers=n_worker, log_level=log_level
             )
         elif args['snpeff']:
             kwargs = {
+                'data_src_dir_path': args['<data_dir_path>'],
                 'normalize_vcf': args['--normalize-vcf'],
                 'ref_version': args['--ref-ver'],
                 'snpeff_db': args['--snpeff-db'],
                 'snpeff': _fetch_snpeff_sh(jar_path=args['--snpeff-jar']),
-                'bcftools': bcftools_path,
+                'bcftools': bcftools,
                 **{c: fetch_executable(c) for c in ['bgzip', 'tabix']},
                 **common_kwargs
             }
             build_luigi_tasks(
                 tasks=[
-                    AnnotateVcfWithSnpeff(
-                        input_vcf_path=str(Path(p).resolve()), **kwargs
-                    ) for p in args['<vcf_path>']
+                    AnnotateVcfWithSnpeff(input_vcf_path=p, **kwargs)
+                    for p in args['<vcf_path>']
+                ],
+                workers=n_worker, log_level=log_level
+            )
+        elif args['funcotator']:
+            kwargs = {
+                'data_src_dir_path': args['<data_dir_path>'],
+                'normalize_vcf': args['--normalize-vcf'],
+                'ref_version': args['--ref-ver'],
+                'gatk': fetch_executable('gatk'), 'bcftools': bcftools,
+                **common_kwargs
+            }
+            build_luigi_tasks(
+                tasks=[
+                    AnnotateVcfWithFuncotator(input_vcf_path=p, **kwargs)
+                    for p in args['<vcf_path>']
                 ],
                 workers=n_worker, log_level=log_level
             )
@@ -172,19 +192,40 @@ def main():
                 'gatk': fetch_executable('gatk'), **common_kwargs
             }
             build_luigi_tasks(
-                tasks=(
-                    [
-                        AnnotateSegWithFuncotateSegments(
-                            input_seg_path=str(Path(p).resolve()), **kwargs
-                        ) for p in args['<seg_path>']
-                    ] if args['funcotatesegments'] else [
-                        AnnotateVcfWithFuncotator(
-                            input_vcf_path=str(Path(p).resolve()),
-                            normalize_vcf=args['--normalize-vcf'],
-                            bcftools=bcftools_path, **kwargs
-                        ) for p in args['<vcf_path>']
-                    ]
+                tasks=[
+                    AnnotateSegWithFuncotateSegments(
+                        input_seg_path=p, **kwargs
+                    ) for p in args['<seg_path>']
+                ],
+                workers=n_worker, log_level=log_level
+            )
+        elif args['stats']:
+            kwargs = {
+                'bcftools': bcftools,
+                'plot_vcfstats': fetch_executable('plot-vcfstats'),
+                **{k: v for k, v in common_kwargs.items() if k != 'memory_mb'}
+            }
+            build_luigi_tasks(
+                tasks=[
+                    CollectVcfStats(input_vcf_path=p, **kwargs)
+                    for p in args['<vcf_path>']
+                ],
+                workers=n_worker, log_level=log_level
+            )
+        elif args['metrics']:
+            kwargs = {
+                'dbsnp_vcf_path': args['<dbsnp_vcf_path>'],
+                'picard': (
+                    fetch_executable('gatk', ignore_errors=True)
+                    or fetch_executable('picard')
                 ),
+                **common_kwargs
+            }
+            build_luigi_tasks(
+                tasks=[
+                    CollectVariantCallingMetrics(input_vcf_path=p, **kwargs)
+                    for p in args['<vcf_path>']
+                ],
                 workers=n_worker, log_level=log_level
             )
 
